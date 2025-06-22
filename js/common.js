deafde938f2fc943d9c55dcb0f7feac983cb0023
Viktoria Zaharova <zaharova181114@gmail.com>
2025-06-23 00:27:34 +0300

    2.3 pc version

--- js/common.js
+++ js/common.js
@@ -1,2 +1,16 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+// slick 
+$('.brand-slider').slick({
+  slidesToShow: 1,
+  fade: true,
+  dots: true,
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>'
+});
+
+
+// Fancybox
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
