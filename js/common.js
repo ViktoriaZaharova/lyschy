6d47144f4248c09fa81f54515362a26ed3bcaf32
Viktoria Zaharova <zaharova181114@gmail.com>
2025-06-25 01:04:37 +0300

    mobile 2/3

--- js/common.js
+++ js/common.js
@@ -2,43 +2,75 @@ $('[name="phone"]').mask('+7 (999) 999-99-99');
 
 // slick 
 $('.brand-slider').slick({
-  slidesToShow: 1,
-  fade: true,
-  dots: true,
-  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
-  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>'
+    slidesToShow: 1,
+    fade: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 1150,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }
+    ]
 });
 
 $('.reviews-slider1').slick({
-  slidesToShow: 3,
-  dots: false,
-  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
-  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>'
+    slidesToShow: 3,
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 1150,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                dots: true
+            }
+        }
+    ]
 });
 
 $('.reviews-slider2').slick({
-  slidesToShow: 3,
-  dots: false,
-  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
-  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>'
+    slidesToShow: 3,
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 1150,
+            settings: {
+                arrows: false,
+                // autoplay: true,
+                autoplaySpeed: 2000,
+                dots: true
+            }
+        }
+    ]
 });
 
 // Fancybox
 Fancybox.bind("[data-fancybox]", {
-  // Your custom options
+    // Your custom options
 });
 
 // accordeon
 function accordeon() {
-	var panel = $('.panel_heading');
+    var panel = $('.panel_heading');
 
-	if (panel.hasClass('in')) {
-		$('.in').find('.block_hover').slideDown();
-	}
+    if (panel.hasClass('in')) {
+        $('.in').find('.block_hover').slideDown();
+    }
 
-	$('.panel_heading .block_title').on('click', function () {
-		$(this).parent().toggleClass('in').find('.block_hover').slideToggle();
-	});
+    $('.panel_heading .block_title').on('click', function () {
+        $(this).parent().toggleClass('in').find('.block_hover').slideToggle();
+    });
 }
 
 accordeon();
@@ -46,12 +78,12 @@ accordeon();
 
 // модальные окна (несколько)
 $(function () {
-    let overlay = $('.overlay'),    
+    let overlay = $('.overlay'),
         open_modal = $('.open_modal'),
         close = $('.modal__close, .overlay'),
         modal = $('.modal__div');
 
-    open_modal.on('click',function (event) {
+    open_modal.on('click', function (event) {
         event.preventDefault();
 
         modal.css('display', 'none').animate({
@@ -74,9 +106,9 @@ $(function () {
     close.on('click', function () {
         modal
             .animate({
-                    opacity: 0,
-                    top: '45%'
-                }, 200,
+                opacity: 0,
+                top: '45%'
+            }, 200,
                 function () {
                     $(this).css('display', 'none');
                     overlay.fadeOut(400);
